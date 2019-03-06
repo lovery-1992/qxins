cf9b0dc01a3735bc05c3b786b7f89e621d3a573b
1300720115@qq.com <1300720115@qq.com>
2019-03-06 17:32:43 +0800

    element-ui vue demo(create)

---
+++ vue/vue-demo/element-ui/src/components/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <el-carousel :interval="4000" type="card" height="600px">
+      <el-carousel-item>
+        <img src="./../assets/logo.png">
+      </el-carousel-item>
+      <el-carousel-item>
+        <img src="./../assets/logo.png">
+      </el-carousel-item>
+      <el-carousel-item>
+        <img src="./../assets/logo.png">
+      </el-carousel-item>
+      <el-carousel-item>
+        <img src="./../assets/logo.png">
+      </el-carousel-item>
+      <el-carousel-item>
+        <img src="./../assets/logo.png">
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
